a8fa906119c5b3bc59e589207c22da0f74eaf630
FIRST_NAME LAST_NAME <outmrhoust.hicham@gmail.com>
2022-09-15 17:29:01 +0100

    added the sign up component , the form input component and the button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,37 @@
-import {signInWithGooglePopup,createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
-const SignIn=()=>{
-    const logGoogleUser = async ()=>{
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
-    return(
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import {
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+const SignIn = () => {
+  /* Checking if the user is already logged in. If the user is logged in, it will create a user
+  document in the firestore database. */
+//   useEffect(() => {
+//     async function getResult(){
+//       const response = await getRedirectResult(auth);
+//       if (response) {
+//         const userDocRef = await createUserDocumentFromAuth(response.user);
+//       }
+//     };
+//     getResult();
+//   }, []);
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+  return (
     <div>
-        <h1>Sign In page</h1>
-        <button onClick={logGoogleUser}>sign in with google popup</button>
+      <h1>Sign In page</h1>
+      <button onClick={logGoogleUser}>sign in with google popup</button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        sign in with google redirect
+      </button> */}
+      <SignUpForm />
     </div>
-        )
-}
-export default SignIn
+  );
+};
+export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
   signInWithPopup,
   signInWithRedirect,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -19,32 +20,45 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 export const db = getFirestore();
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation={}
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, "users", userAuth.uid);
-  console.log(userDocRef);
+
   const userSnapshot = await getDoc(userDocRef);
 
-  if (!userSnapshot.exists()){
-    const {displayName,email}=userAuth;
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
-    try {
-        await setDoc(userDocRef,{
-            displayName,
-            email,
-            createdAt
-        })
-    }catch(err){
-        console.log('there was an error',err.message)
-    }
 
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (err) {
+      console.log("there was an error", err.message);
+    }
   }
-  return userDocRef
+  return userDocRef;
+};
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
