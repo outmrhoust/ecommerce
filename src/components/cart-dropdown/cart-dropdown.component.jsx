39284d814911f034d030e3a26357627cca38eb78
FIRST_NAME LAST_NAME <outmrhoust.hicham@gmail.com>
2022-10-10 12:37:52 +0100

    added styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-=======
-import "./cart-dropdown.styles.scss";
->>>>>>> ebc06ffc2ed5831344d39267163f483f10bf5046
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
-<<<<<<< HEAD
 import { CartDropdownContainer,CartItems } from "./cart-dropdown.styles";
 
-=======
->>>>>>> ebc06ffc2ed5831344d39267163f483f10bf5046
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
 
@@ -22,7 +15,6 @@ const CartDropdown = () => {
   };
 
   return (
-<<<<<<< HEAD
     <CartDropdownContainer>
       <CartItems>
         {cartItems.map((item) => (
@@ -31,16 +23,6 @@ const CartDropdown = () => {
       </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </CartDropdownContainer>
-=======
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
-      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
->>>>>>> ebc06ffc2ed5831344d39267163f483f10bf5046
   );
 };
 export default CartDropdown;
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -5,11 +5,7 @@ import ProductCard from '../../components/product-card/product-card.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
-<<<<<<< HEAD
 import {CategoryContainer,CategoryTitle} from './category.styles';
-=======
-import './category.styles.scss';
->>>>>>> ebc06ffc2ed5831344d39267163f483f10bf5046
 
 const Category = () => {
   const { category } = useParams();
@@ -22,22 +18,13 @@ const Category = () => {
 
   return (
     <Fragment>
-<<<<<<< HEAD
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
       <CategoryContainer>
-=======
-      <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
->>>>>>> ebc06ffc2ed5831344d39267163f483f10bf5046
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-<<<<<<< HEAD
       </CategoryContainer>
-=======
-      </div>
->>>>>>> ebc06ffc2ed5831344d39267163f483f10bf5046
     </Fragment>
   );
 };
